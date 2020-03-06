a42636d2fb874df6fa156e2b385c8dd56a0bffac
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-06 17:25:22 +0530

    REFACTORED : passing message from constructor

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,5 +1,23 @@
 public class MoodAnalyser {
+    //variables
+    private String message;
+    //no arg constructor
+    public MoodAnalyser(){
+
+    }
+    //constructor with parameter
+    public MoodAnalyser(String message)
+    {
+        this.message=message;
+    }
+    //method for accessing parameters
     public String analyseMood(String message)
+    {
+        this.message=message;
+        return analyseMood();
+    }
+    //analyseMood return SAD or HAPPY
+    public String analyseMood()
     {
         if(message.equals("i am in sad mood"))
         {
