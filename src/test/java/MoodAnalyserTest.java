cdd63228063e0035db13be71ed51d40cabffa696
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-06 17:28:41 +0530

    REFACTORED : mood message in Constructor to return sad

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,20 +4,18 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     private MoodAnalyser moodAnalyser;
     private String result;
-    //test case for return sad
     @Test
     public void givenMessage_WhenAnalyse_ThenSadMood()
     {
-        moodAnalyser=new MoodAnalyser();
-        result=moodAnalyser.analyseMood("i am in sad mood");
+        moodAnalyser=new MoodAnalyser("i am in sad mood");
+        result=moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",result);
     }
-    //analyseMood method can just return happy to test else message
     @Test
     public void givenMessage_WhenAnalyse_ThenHappyMood()
     {
         moodAnalyser=new MoodAnalyser();
-        result=moodAnalyser.analyseMood("“I am in Any Mood”");
+        result=moodAnalyser.analyseMood("I am in Any Mood");
         Assert.assertEquals("HAPPY",result);
     }
 }
