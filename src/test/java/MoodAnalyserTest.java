61a5bb5567842755e878ca107b3789e57ac80791
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-06 17:17:59 +0530

    ADDED : analyseMood method can just return happy to test else happy message

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -12,4 +12,12 @@ public class MoodAnalyserTest {
         result=moodAnalyser.analyseMood("i am in sad mood");
         Assert.assertEquals("SAD",result);
     }
+    //analyseMood method can just return happy to test else message
+    @Test
+    public void givenMessage_WhenAnalyse_ThenHappyMood()
+    {
+        moodAnalyser=new MoodAnalyser();
+        result=moodAnalyser.analyseMood("“I am in Any Mood”");
+        Assert.assertEquals("HAPPY",result);
+    }
 }
