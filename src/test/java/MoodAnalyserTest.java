d8c449e75ab41a24f1afb54e371c2aec2d226631
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-06 17:16:04 +0530

    ADDED : analyseMood method can just return SAD to pass this Test Case

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyserTest {
+    private MoodAnalyser moodAnalyser;
+    private String result;
+    //test case for return sad
+    @Test
+    public void givenMessage_WhenAnalyse_ThenSadMood()
+    {
+        moodAnalyser=new MoodAnalyser();
+        result=moodAnalyser.analyseMood("i am in sad mood");
+        Assert.assertEquals("SAD",result);
+    }
 }
