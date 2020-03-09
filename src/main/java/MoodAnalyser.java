6bd86e955246639e69b9192cddfe4a7d7d712090
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-09 12:33:30 +0530

    REFACTORED : shift brackets and changed code formate

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,31 +1,26 @@
 public class MoodAnalyser {
     //variables
     private String message;
-    //no arg constructor
-    public MoodAnalyser(){
 
-    }
+    //no arg constructor
+    public MoodAnalyser(){ }
+
     //constructor with parameter
-    public MoodAnalyser(String message)
-    {
+    public MoodAnalyser(String message) {
         this.message=message;
     }
+
     //method for accessing parameters
-    public String analyseMood(String message)
-    {
+    public String analyseMood(String message) {
         this.message=message;
         return analyseMood();
     }
+
     //analyseMood return SAD or HAPPY
-    public String analyseMood()
-    {
+    public String analyseMood() {
         if(message.contains("sad"))
-        {
             return "SAD";
-        }
         else
-        {
             return "HAPPY";
-        }
     }
 }
