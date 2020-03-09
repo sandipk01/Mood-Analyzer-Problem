77457acb2a1bb3faa1aa49e99079dda90ce9a649
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-09 15:41:24 +0530

    ADDED : Given null message then should throw null pointer exception

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -6,23 +6,27 @@ public class MoodAnalyserTest {
     private String result;
 
     @Test
-    public void givenMessage_WhenAnalyse_ThenSadMood(){
+    public void givenMessage_WhenAnalyse_ThenSadMood() throws MoodAnalysisException {
         moodAnalyser=new MoodAnalyser("I am in sad mood");
         result=moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",result);
     }
 
     @Test
-    public void givenMessage_WhenAnalyse_ThenHappyMood(){
+    public void givenMessage_WhenAnalyse_ThenHappyMood() throws MoodAnalysisException {
         moodAnalyser=new MoodAnalyser("I am in Any Mood");
         result=moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",result);
     }
 
     @Test
-    public void givenNull_WhenNullPointerException_ThenHappy(){
-        moodAnalyser=new MoodAnalyser(null);
-        result=moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",result);
+    public void givenNullMessageShouldThrowNullPointerException() {
+        try {
+            moodAnalyser=new MoodAnalyser(null);
+            result=moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NULL_EXCEPTION,e.typeOfException);
+        }
     }
+
 }
