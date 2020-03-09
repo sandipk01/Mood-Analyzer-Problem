f84e1df1add5d9da6ef2300edf9509547f872f98
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-09 12:35:20 +0530

    REFACTORED : shift brackets and changed code formate

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     private MoodAnalyser moodAnalyser;
     private String result;
+
     @Test
-    public void givenMessage_WhenAnalyse_ThenSadMood()
-    {
+    public void givenMessage_WhenAnalyse_ThenSadMood(){
         moodAnalyser=new MoodAnalyser("i am in sad mood");
         result=moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",result);
     }
+
     @Test
-    public void givenMessage_WhenAnalyse_ThenHappyMood()
-    {
+    public void givenMessage_WhenAnalyse_ThenHappyMood(){
         moodAnalyser=new MoodAnalyser("I am in Any Mood");
         result=moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",result);
