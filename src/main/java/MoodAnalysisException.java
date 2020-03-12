c9b3defba1a98f5f431b92dc9bcf17979b1601cd
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-12 13:46:39 +0530

    ADDED : No such Method type is added

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,10 +1,11 @@
 public class MoodAnalysisException extends Exception {
+    //types of Exceptions
     enum TypeOfException {
-        NULL_EXCEPTION,EMPTY_EXCEPTION,NO_SUCH_CLASS,No_SUCH_METHOD
+        NULL_EXCEPTION,EMPTY_EXCEPTION,NO_SUCH_CLASS,NO_SUCH_METHOD
     }
-
+    //declared exception
     TypeOfException typeOfException;
-
+    //parameterized constructor
     public MoodAnalysisException(TypeOfException typeOfException,String message){
         super(message);
         this.typeOfException=typeOfException;
