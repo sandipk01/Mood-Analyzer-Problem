8631cb460934af70dcd350b21517760885a7b5ab
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-12 13:23:39 +0530

    REFACTORED : Class not found exception throws user defined exception

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,25 +1,29 @@
+import sun.tools.java.ClassNotFound;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
 
-    public static MoodAnalyser createMoodAnalyser(){
+    public static MoodAnalyser createMoodAnalyser() throws MoodAnalysisException {
         try {
             Class moodClass=Class.forName("MoodAnalyser");
             Constructor constructor=moodClass.getConstructor();
             Object moodObject=constructor.newInstance();
             return  (MoodAnalyser) moodObject;
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS,"class not found");
         } catch (IllegalAccessException | InstantiationException |InvocationTargetException e) {
             e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+
         }
         return null;
     }
 
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        Object object=MoodAnalyserFactory.createMoodAnalyser();
-        System.out.println(object.equals(moodAnalyser));
+    public static void main(String[] args) throws MoodAnalysisException {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Object object = createMoodAnalyser();
+            System.out.println(object.equals(moodAnalyser));
     }
 }
