3c63e62838378060b7358425531450b2b960e58d
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-12 12:16:36 +0530

    REFACTORED : MoodAnalyserFactory is added createMoodAnalyser method to return a MoodAnalyser Object

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserFactory {
+
+    public static MoodAnalyser createMoodAnalyser(){
+        try {
+            Class moodClass=Class.forName("MoodAnalyser");
+            Constructor constructor=moodClass.getConstructor();
+            Object moodObject=constructor.newInstance();
+            return  (MoodAnalyser) moodObject;
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException | InstantiationException |InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser=new MoodAnalyser();
+        Object object=MoodAnalyserFactory.createMoodAnalyser();
+        System.out.println(object.equals(moodAnalyser));
+    }
 }
