dac8bbd7118211d03491c95eb39cd91889cfb976
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-12 13:43:39 +0530

    REFACTORED : No such method user defined exception is thrown

--- src/main/java/MoodAnalyserFactory.java
+++ src/main/java/MoodAnalyserFactory.java
@@ -2,21 +2,22 @@ import sun.tools.java.ClassNotFound;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.time.format.TextStyle;
 
 public class MoodAnalyserFactory {
-
+    //this method returns the Object od MoodAnalyser
     public static MoodAnalyser createMoodAnalyser() throws MoodAnalysisException {
         try {
             Class moodClass=Class.forName("MoodAnalyser");
-            Constructor constructor=moodClass.getConstructor();
+            Constructor constructor=moodClass.getConstructor(TextStyle.class);
             Object moodObject=constructor.newInstance();
             return  (MoodAnalyser) moodObject;
         } catch (ClassNotFoundException e) {
-            throw new MoodAnalysisException(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS,"class not found");
+            throw new MoodAnalysisException(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS,"No such class found");
         } catch (IllegalAccessException | InstantiationException |InvocationTargetException e) {
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
-
+            throw new MoodAnalysisException(MoodAnalysisException.TypeOfException.NO_SUCH_METHOD,"No such method found");
         }
         return null;
     }
