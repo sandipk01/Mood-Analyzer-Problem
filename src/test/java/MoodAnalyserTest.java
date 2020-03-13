5fa990a4d37e8c309714c531f537026bf606e1a2
sadnipk01 <sadnipkenagar01@gmail.com>
2020-03-13 10:43:20 +0530

    ADDED : Given improper constructor parameter should throw no such method exception

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -13,93 +13,100 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMessage_WhenAnalyse_ThenSadMood() throws MoodAnalysisException {
-        moodAnalyser=new MoodAnalyser("I am in sad mood");
-        result=moodAnalyser.analyseMood();
-        Assert.assertEquals("SAD",result);
+        moodAnalyser = new MoodAnalyser("I am in sad mood");
+        result = moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD", result);
     }
 
     @Test
     public void givenMessage_WhenAnalyse_ThenHappyMood() throws MoodAnalysisException {
-        moodAnalyser=new MoodAnalyser("I am in Any Mood");
-        result=moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",result);
+        moodAnalyser = new MoodAnalyser("I am in Any Mood");
+        result = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", result);
     }
 
     @Test
     public void givenNullMessageShouldReturnHappy() {
         try {
-            moodAnalyser=new MoodAnalyser(null);
-            result=moodAnalyser.analyseMood();
+            moodAnalyser = new MoodAnalyser(null);
+            result = moodAnalyser.analyseMood();
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals("HAPPY",result);
+            Assert.assertEquals("HAPPY", result);
         }
     }
 
     @Test
     public void givenNullMessageShouldThrowNullPointerException() {
         try {
-            moodAnalyser=new MoodAnalyser(null);
+            moodAnalyser = new MoodAnalyser(null);
             moodAnalyser.analyseMood();
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.TypeOfException.NULL_EXCEPTION,e.typeOfException);
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NULL_EXCEPTION, e.typeOfException);
         }
     }
 
     @Test
-    public void givenMessageEmptyShouldThrowsEmptyException(){
+    public void givenMessageEmptyShouldThrowsEmptyException() {
         try {
-            moodAnalyser=new MoodAnalyser("");
+            moodAnalyser = new MoodAnalyser("");
             moodAnalyser.analyseMood();
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.TypeOfException.EMPTY_EXCEPTION,e.typeOfException);
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.EMPTY_EXCEPTION, e.typeOfException);
         }
     }
 
     @Test
     public void givenTwoObjects_WhenEquals_ThenShouldReturnTrue() throws MoodAnalysisException {
-        moodAnalyser=new MoodAnalyser();
-        constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser");
-        object=MoodAnalyserFactory.createMoodAnalyser(constructor);
-        resultBoolean=moodAnalyser.equals(object);
+        moodAnalyser = new MoodAnalyser();
+        constructor = MoodAnalyserFactory.getConstructor("MoodAnalyser");
+        object = MoodAnalyserFactory.createMoodAnalyser(constructor);
+        resultBoolean = moodAnalyser.equals(object);
         Assert.assertTrue(resultBoolean);
     }
 
     @Test
-    public void givenImproperClass_ThenShouldThrowNoSuchClassException(){
-        moodAnalyser=new MoodAnalyser();
+    public void givenImproperClass_ThenShouldThrowNoSuchClassException() {
+        moodAnalyser = new MoodAnalyser();
         try {
-            constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser");
+            constructor = MoodAnalyserFactory.getConstructor("MoodAnalyse");
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS,e.typeOfException);
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS, e.typeOfException);
         }
     }
 
     @Test
     public void givenImproperConstructorParameter_ThenShouldThrowNoSuchMethodException() {
         try {
-            constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser",Object.class);
+            constructor = MoodAnalyserFactory.getConstructor("MoodAnalyser",Integer.class);
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_METHOD,e.typeOfException);
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_METHOD, e.typeOfException);
         }
     }
 
     @Test
     public void givenTwoParameterizeObject_WhenEquals_ThenShouldReturnsTrue() throws MoodAnalysisException {
-        moodAnalyser=new MoodAnalyser("sad");
-        constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser",String.class);
-        object=MoodAnalyserFactory.createMoodAnalyser(constructor,"sad");
-        resultBoolean=moodAnalyser.equals(object);
+        moodAnalyser = new MoodAnalyser("sad");
+        constructor = MoodAnalyserFactory.getConstructor("MoodAnalyser", String.class);
+        object = MoodAnalyserFactory.createMoodAnalyser(constructor, "sad");
+        resultBoolean = moodAnalyser.equals(object);
         Assert.assertTrue(resultBoolean);
     }
 
     @Test
     public void givenImproperClass_ThenShouldThrowNoClassFoundException() {
         try {
-            constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser",String.class);
-            object=MoodAnalyserFactory.createMoodAnalyser(constructor,"I am in sad mood");
+            constructor = MoodAnalyserFactory.getConstructor("MoodAnalyser",String.class);
         } catch (MoodAnalysisException e) {
-            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS,e.typeOfException);
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_CLASS, e.typeOfException);
         }
+    }
 
+    @Test
+    public void givenImproperConstructorParameter_ThenShouldThrowsNoSuchMethodException() {
+        try {
+            constructor=MoodAnalyserFactory.getConstructor("MoodAnalyser",Integer.class,String.class);
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.TypeOfException.NO_SUCH_METHOD,e.typeOfException);
+        }
     }
 }
